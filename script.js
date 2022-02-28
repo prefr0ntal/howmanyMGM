05dc4cda9ffe349fd81a023d3456164537421ddc
prefr0ntal <100566887+prefr0ntal@users.noreply.github.com>
2022-02-28 20:03:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 let needpiece = [0, 50, 100, 200, 300];
 let nullObj = {};
-let n;
 addEventListener('load', listener);
 addEventListener('click', listener);
 addEventListener('keyup', listener);
 let ps = [nullObj, two, three, four, five];
 function listener() {
-  n = Array(5)
-    .fill(0)
-    .map((v, i) => {
+  let n = Array(5).fill(0).map((v, i) => {
       if (star.value <= 0 || i + 1 <= star.value) return 0;
       return needpiece.slice(star.value, i + 1).reduce((a, b) => a + b);
-    });
+  });
   ps.forEach((v, i) => {
     let num = piece.value - 0;
     if(num<0)return v.innerText=0
     if (isNaN(num)) num = 0;
-    let namb=n[i] - num
-    v.innerText = namb<0?0:namb;
+    v.innerText =(n[i]-num<0?)0:n[i]-num;
   });
   [nullObj, retwo, rethree, refour, refive].forEach((v, i) => {
     if ([piece.value, i, mpp.value, k.value].some((v) => v == '')) return;
@@ -25,10 +21,7 @@ function listener() {
   });
 }
 function keisan(piece, lv, mpp, kk) {
-  if ([lv, mpp, kk].some((v) => isNaN(v))) return;
-  if (isNaN(piece)) piece = 0;
-  if (n[lv] == 0) return 0;
-  if (n[lv] <= piece) return 0
+  if ([piece,lv, mpp, kk].some((v) => isNaN(v))||n[lv] == 0||n[lv] <= piece) return 0;
   let mgm = 0;
   let gag = 0;
   while (true) {
